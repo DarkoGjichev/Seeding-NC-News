fb134d4dbf4fc0745bf215a7636dbe362b77f151
DarkoGjichev <darkogicev111@gmail.com>
2025-07-18 11:25:53 +0100

    added values to the tables

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,5 +1,6 @@
 const db = require("../connection");
 var format = require("pg-format");
+const { convertTimestampToDate, createLookupRef } = require("./utils.js");
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
   return db
@@ -50,21 +51,72 @@ const seed = ({ topicData, userData, articleData, commentData }) => {
       )`);
     })
     .then(() => {
-      const mainArr = [];
-      topicData.map((topic) => {
-        const nestedArr = [];
-        for (let key in topic) {
-          nestedArr.push(topic[key]);
-        }
-        mainArr.push(nestedArr);
+      const formattedData = topicData.map((topic) => {
+        return [topic.description, topic.slug, topic.img_url];
       });
       const topicsData = format(
         `INSERT INTO topics (description, slug, img_url)
       VALUES %L
       RETURNING *;`,
-        mainArr
+        formattedData
       );
       return db.query(topicsData);
+    })
+    .then(() => {
+      const formattedData = userData.map((user) => {
+        return [user.username, user.name, user.avatar_url];
+      });
+      const usersData = format(
+        `INSERT INTO users (username, name, avatar_url)
+      VALUES %L
+      RETURNING *;`,
+        formattedData
+      );
+      return db.query(usersData);
+    })
+    .then(() => {
+      const formattedData = articleData.map((article) => {
+        const convertedTimestamp = convertTimestampToDate(article);
+        return [
+          article.title,
+          article.topic,
+          article.author,
+          article.body,
+          convertedTimestamp.created_at,
+          article.votes,
+          article.article_img_url,
+        ];
+      });
+      const articlesData = format(
+        `INSERT INTO articles (title, topic, author, body, created_at, votes, article_img_url)
+      VALUES %L
+      RETURNING *;`,
+        formattedData
+      );
+      return db.query(articlesData);
+    })
+    .then(({ rows: articles }) => {
+      const ref = createLookupRef(articles, "title", "article_id");
+      const formattedData = commentData.map((comment) => {
+        const convertedTimestamp = convertTimestampToDate(comment);
+        return [
+          ref[comment.article_title],
+          comment.body,
+          comment.votes,
+          comment.author,
+          convertedTimestamp.created_at,
+        ];
+      });
+      const commentsData = format(
+        `INSERT INTO comments (article_id, body, votes, author,created_at)
+      VALUES %L
+      RETURNING *;`,
+        formattedData
+      );
+      return db.query(commentsData);
+    })
+    .catch((err) => {
+      console.error(err);
     });
 };
 
