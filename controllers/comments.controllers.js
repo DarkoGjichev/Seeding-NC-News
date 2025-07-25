362679fdaf0c77180cd1edddf34bc414982f5647
DarkoGjichev <darkogicev111@gmail.com>
2025-07-25 11:50:15 +0100

    invokes function fetchArticleById so fetch new comments can reuse it and check that ID exists before searching for comments for that ID

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -1,3 +1,4 @@
+const { fetchArticleById } = require("../models/articles.models.js");
 const {
   fetchCommentsByArticleId,
   attachNewComment,
@@ -5,9 +6,15 @@ const {
 
 const getCommentsByArticleId = (req, res, next) => {
   const { article_id } = req.params;
-  fetchCommentsByArticleId(article_id)
-    .then((comments) => {
-      res.status(200).send({ comments });
+  fetchArticleById(article_id)
+    .then((article_id) => {
+      fetchCommentsByArticleId(article_id)
+        .then((comments) => {
+          res.status(200).send({ comments });
+        })
+        .catch((err) => {
+          next(err);
+        });
     })
     .catch((err) => {
       next(err);
--- models/comments.models.js
+++ models/comments.models.js
@@ -1,18 +1,12 @@
 const db = require("../db/connection.js");
 
-const fetchCommentsByArticleId = (article_id) => {
+const fetchCommentsByArticleId = ({ article_id }) => {
   return db
     .query(
       "SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC",
       [article_id]
     )
     .then(({ rows: comments }) => {
-      if (comments.length === 0) {
-        return Promise.reject({
-          status: 404,
-          msg: "No Comments found for this article",
-        });
-      }
       return comments;
     });
 };
