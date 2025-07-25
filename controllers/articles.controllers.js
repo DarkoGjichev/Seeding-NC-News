2a20a7daaaba839219e83d73dc9e3e54353f97ea
DarkoGjichev <darkogicev111@gmail.com>
2025-07-25 12:24:58 +0100

    Removed catch blocks for errors from controller functions

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -4,24 +4,16 @@ const {
 } = require("../models/articles.models");
 
 const getAllArticles = (req, res) => {
-  fetchAllArticles()
-    .then((articles) => {
-      res.status(200).send({ articles });
-    })
-    .catch((err) => {
-      next(err);
-    });
+  return fetchAllArticles().then((articles) => {
+    res.status(200).send({ articles });
+  });
 };
 
-const getArticleById = (req, res, next) => {
+const getArticleById = (req, res) => {
   const { article_id } = req.params;
-  fetchArticleById(article_id)
-    .then((article) => {
-      res.status(200).send({ article });
-    })
-    .catch((err) => {
-      next(err);
-    });
+  return fetchArticleById(article_id).then((article) => {
+    res.status(200).send({ article });
+  });
 };
 
 module.exports = { getAllArticles, getArticleById };
--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -4,33 +4,21 @@ const {
   attachNewComment,
 } = require("../models/comments.models.js");
 
-const getCommentsByArticleId = (req, res, next) => {
+const getCommentsByArticleId = (req, res) => {
   const { article_id } = req.params;
-  fetchArticleById(article_id)
-    .then((article_id) => {
-      fetchCommentsByArticleId(article_id)
-        .then((comments) => {
-          res.status(200).send({ comments });
-        })
-        .catch((err) => {
-          next(err);
-        });
-    })
-    .catch((err) => {
-      next(err);
+  return fetchArticleById(article_id).then((article_id) => {
+    return fetchCommentsByArticleId(article_id).then((comments) => {
+      res.status(200).send({ comments });
     });
+  });
 };
 
-const addNewComment = (req, res, next) => {
+const addNewComment = (req, res) => {
   const { article_id } = req.params;
   const sentComment = req.body;
-  attachNewComment(article_id, [sentComment])
-    .then((postedComment) => {
-      res.status(201).send({ postedComment });
-    })
-    .catch((err) => {
-      next(err);
-    });
+  return attachNewComment(article_id, [sentComment]).then((postedComment) => {
+    res.status(201).send({ postedComment });
+  });
 };
 
 module.exports = { getCommentsByArticleId, addNewComment };
